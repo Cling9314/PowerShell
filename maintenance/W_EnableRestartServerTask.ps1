807874b6931543cacd98ce68c08f8d56e4f0ad68
Foxxter <matthias.fuchs.los@gmail.com>
2025-04-11 12:57:15 +0200

    Update W_EnableRestartServerTask.ps1

--- maintenance/W_EnableRestartServerTask.ps1
+++ maintenance/W_EnableRestartServerTask.ps1
@@ -1,4 +1,17 @@
-﻿# Überprüfen, ob das Skript mit Administratorrechten ausgeführt wird
+﻿<#
+    Dieses Skript überprüft, ob es mit **Administratorrechten** ausgeführt wird. Falls nicht, wird eine Fehlermeldung ausgegeben
+    und das Skript beendet. Wenn das Skript mit Administratorrechten ausgeführt wird, sucht es nach einem geplanten Task 
+    mit dem angegebenen Namen und aktiviert ihn, falls er gefunden wird.
+
+    Was manuell angepasst werden muss:
+    1. **$TaskName**: Der Name des geplanten Tasks, der aktiviert werden soll. Der Standardname im Skript ist „ServerNeustart_19Uhr“.
+       Wenn der geplante Task einen anderen Namen hat, ändere die Variable `$TaskName` entsprechend.
+       
+    Hinweis: Das Skript prüft, ob der Task mit dem angegebenen Namen existiert. Wenn der Task gefunden wird, wird er aktiviert. 
+    Andernfalls wird eine Nachricht ausgegeben, dass der Task nicht gefunden wurde.
+#>
+
+# Überprüfen, ob das Skript mit Administratorrechten ausgeführt wird
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "Dieses Skript erfordert Administratorrechte. Bitte führen Sie es als Administrator aus."
     exit
